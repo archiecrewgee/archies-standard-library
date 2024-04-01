4771fb3bae99c2138e7c1f2d0d3a16a334728beb
archiecrewgee <archiecg@hotmail.co.nz>
2024-04-01 18:42:41 +0200

    added memory io block/byte read/write functionality. added helper directory containing macros

--- explorations/memory_io.c
+++ explorations/memory_io.c
@@ -4,6 +4,9 @@
 /* Internal Includes */
 #include "memory_io.h"
 
+#include "macros.h"
+#include "memory.h"
+
 /* Structures */
 
 /* Constants */
@@ -12,6 +15,22 @@
 static uint32_t _byte_to_block_address(MemoryIO_Paradigm_t * paradigm, uint32_t address);   // converts a byte address to a block
 static uint32_t _block_to_byte_address(MemoryIO_Paradigm_t * paradigm, uint32_t address);   // converts a block address to a byte
 
+// updates the current state of the byte-by-byte processor
+// byte-by-byte interaction is performed with block processing acting as a state machine:
+//  1. partial interaction (offset from start) 
+//  2. complete block interaction
+//  3. partial interaction (zero offset from start)
+//  4. exit state
+// initial state is defined as so:
+//  1 = address not alligned with block boundry AND NOT remaining length (to read) is less than the size of a block  
+//  2 = absence of other states
+//  3 = remaining length (to read) is less than the size of a block
+//  4 = no other conditions met AND (current state is [3] OR remaining length is zero) 
+// given this from all start positions, noting that 2 state can always go to itself
+//  1 -> 2 ->? 3 -> 4
+//  2 ->? 3 -> 4
+//  3 -> 4
+static uint32_t _update_byte_processor_state(MemoryIO_Paradigm_t * paradigm,  uint32_t startAddress,uint32_t remainingLen, uint32_t currentState);
 
 /* Public Function Definiton */
 int memory_io_bytes_set(MemoryIO_Paradigm_t * paradigm, uint32_t address, uint8_t * src, uint32_t len) {
@@ -22,19 +41,80 @@ int memory_io_bytes_set(MemoryIO_Paradigm_t * paradigm, uint32_t address, uint8_
 }
 
 int memory_io_bytes_get(MemoryIO_Paradigm_t * paradigm, uint32_t address, uint8_t * dst, uint32_t len) {
-    // introduce variables for clarity
-    const uint32_t  startBlock =  _byte_to_block_address(paradigm, address),
-                    endBlock = _byte_to_block_address(paradigm, address + len);
+    // introduce local variables
+    uint32_t currentBlock = _byte_to_block_address(paradigm, address);
+    uint32_t remainingLen = len, processedLen;
+    uint32_t state = _update_byte_processor_state(paradigm, address, remainingLen, 0);
 
-    // for all blocks...
-    for (uint32_t activeBlock = startBlock; activeBlock <= endBlock; activeBlock += paradigm->block.increment, dst += paradigm->block.size) {
+    // itterate through all blocks 
+    while (state < 4) {
+        // read current block
+        asl_assert_r(paradigm->read_block(currentBlock, paradigm->scratch), 1);
 
+        // based on current state...
+        switch (state) {
+            case 1:
+                processedLen = paradigm->block.size - (address % paradigm->block.size);
+                copy(dst, paradigm->scratch + (address % paradigm->block.size), processedLen);
+                break;
+            case 2:
+                processedLen = paradigm->block.size;
+                copy(dst, paradigm->scratch, processedLen);
+                break;
+            case 3:
+                processedLen = remainingLen;
+                copy(dst, paradigm->scratch + paradigm->block.size - remainingLen, processedLen);
+                break;
+            default:
+                break;
+        }
+
+        // itterate active block, the remaining length to calculate and the destination pointer
+        currentBlock += paradigm->block.increment;
+        remainingLen -= processedLen;
+        dst += processedLen;
+
+        _update_byte_processor_state(paradigm, address, remainingLen, state);
     }
-
 
     return 0;
 }
 
+int memory_io_blocks_set(MemoryIO_Paradigm_t * paradigm, uint32_t address, uint8_t * src, uint32_t len) {
+    uint32_t endAddress = address + len;
+
+    // for all blocks write to the memory paradigm IO functionality (asserting zero value) from the src pointer
+    for (; address < endAddress; address += paradigm->block.increment, src += paradigm->block.size) {
+        asl_assert_r(paradigm->read_block(address, src), 1);
+    }
+
+    return 0;
+}
+
+int memory_io_blocks_get(MemoryIO_Paradigm_t * paradigm, uint32_t address, uint8_t * dst, uint32_t len) {
+    uint32_t endAddress = address + len;
+
+    // for all blocks read from the memory paradigm IO functionality (asserting zero value) and add to the destination pointer
+    for (; address < endAddress; address += paradigm->block.increment, dst += paradigm->block.size) {
+        asl_assert_r(paradigm->read_block(address, dst), 1);
+    }
+
+    return 0;
+}
+
+
 /* Private Function Definiton */
 static uint32_t _byte_to_block_address(MemoryIO_Paradigm_t * paradigm, uint32_t address) { return (address / paradigm->block.size) * paradigm->block.increment; }
 static uint32_t _block_to_byte_address(MemoryIO_Paradigm_t * paradigm, uint32_t address) { return (address / paradigm->block.increment) * paradigm->block.size; }
+
+static uint32_t _update_byte_processor_state(MemoryIO_Paradigm_t * paradigm, uint32_t startAddress, uint32_t remainingLen, uint32_t currentState) {
+    if (currentState == 3 || !remainingLen) {
+        return 4;
+    } else if (remainingLen < paradigm->block.size) {
+        return 3;
+    } else if (startAddress % paradigm->block.size) {
+        return 2;
+    } 
+    
+    return 1;
+}
--- explorations/memory_io.h
+++ explorations/memory_io.h
@@ -10,13 +10,17 @@
 /* Structures and Definitions */
 
 /**
- * @brief Type defining a generic 'write memory block' function. This interface should remain the same regardless of the memory paradigm.
+ * @brief 
+ *  Type defining a generic 'write memory block' function. This interface should remain the same regardless of the memory paradigm.
+ *  Non-zero return value is considered a failure
  * @note This function is treated as blocking
  */
 typedef int (MemoryIO_WriteBlock_f)(uint32_t blockAddress, uint8_t * src);
 
 /**
- * @brief Type defining a generic 'read memory block' function. This interface should remain the same regardless of the memory paradigm. 
+ * @brief 
+ *  Type defining a generic 'read memory block' function. This interface should remain the same regardless of the memory paradigm. 
+ *  Non-zero return value is considered a failure
  * @note This function is treated as blocking
  */
 typedef int (MemoryIO_ReadBlock_f)(uint32_t blockAddress, uint8_t * dst);
@@ -35,7 +39,7 @@ typedef const struct {
     MemoryIO_WriteBlock_f write_block;
     MemoryIO_ReadBlock_f read_block;
     
-    uint8_t * scratch;  // this should be allocated dynamically/statically a location in memory of the same size as a block
+    uint8_t * scratch;  // this should be allocated dynamically/statically a location in memory of the same size as a given paradigm's block
 } MemoryIO_Paradigm_t;
 
 /* Constants */
@@ -48,8 +52,8 @@ typedef const struct {
  * @param paradigm pointer to memory paradigm instance
  * @param address byte address to be written to
  * @param src pointer to the source data to be written
- * @param len the number of bytes to writen
- * @return int return value; 0 on success and 1 on failure (note that a failure can only occur in the block IO functionality)
+ * @param len the number of bytes to write
+ * @return int return value; 0 on success and 1 on failure (note that a failure is only asserted in the block IO functionality)
  */
 int memory_io_bytes_set(MemoryIO_Paradigm_t * paradigm, uint32_t address, uint8_t * src, uint32_t len);
 
@@ -60,8 +64,30 @@ int memory_io_bytes_set(MemoryIO_Paradigm_t * paradigm, uint32_t address, uint8_
  * @param address byte address to be read from
  * @param dst pointer to the dst data to be read to
  * @param len the number of bytes to read
- * @return int return value; 0 on success and 1 on failure (note that a failure can only occur in the block IO functionality)
+ * @return int return value; 0 on success and 1 on failure (note that a failure is only asserted in the block IO functionality)
  */
 int memory_io_bytes_get(MemoryIO_Paradigm_t * paradigm, uint32_t address, uint8_t * dst, uint32_t len);
+
+/**
+ * @brief Reads len blocks from the given memory paradigm to dst
+ * 
+ * @param paradigm pointer to the memory paradigm instance
+ * @param address block address to be read from
+ * @param dst pointer to the dst data to be read to
+ * @param len the number of blocks to read
+ * @return int return value; 0 on success and 1 on failure (note that a failure is only asserted in the block IO functionality)
+ */
+int memory_io_blocks_get(MemoryIO_Paradigm_t * paradigm, uint32_t address, uint8_t * dst, uint32_t len);
+
+/**
+ * @brief Writes len blocks from src to the given memory paradigm
+ * 
+ * @param paradigm pointer to memory paradigm instance
+ * @param address block address to be written to
+ * @param src pointer to the source data to be written
+ * @param len the number of blocks to write
+ * @return int return value; 0 on success and 1 on failure (note that a failure is only asserted in the block IO functionality)
+ */
+int memory_io_blocks_set(MemoryIO_Paradigm_t * paradigm, uint32_t address, uint8_t * src, uint32_t len);
 
 #endif
