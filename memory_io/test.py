e79e16b115a8b53b4ceba011392e614cc6bf8fbe
archiecrewgee <archiecg@hotmail.co.nz>
2024-05-10 10:29:17 +0200

    [wip] - start of development syn

--- memory_io/test.py
+++ memory_io/test.py
@@ -1,6 +1,23 @@
 import yaml
+from dataclasses import dataclass
+
+@dataclass
+class Variable():
+    name: str
+    size: int
+    defined = []
+    
+    def __init__(self, variable: dict):
+        self.name = list(variable.keys())[0]
 
 
 with open("example_memory_map.yaml", 'r') as stream:
-    data_loaded = yaml.safe_load(stream)
-print(data_loaded)
+    data = yaml.safe_load(stream)
+
+print(data)
+
+
+for variable in data["parameters"]:
+    print(variable) 
+    something = Variable(variable, size=variable["size"])
+    print(something)
